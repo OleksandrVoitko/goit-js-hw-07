ebbb2581932e043537a6128be54535350fddf10d
OleksandrVoitko <voytkomail@gmail.com>
2021-10-19 17:29:57 +0300

    Создание и рендер разметки

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+
+const refs = {
+    divGallery: document.querySelector('.gallery'),
+};
+
+const itemsImages = galleryItems.map((item) => {
+  return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}}"
+            alt="${item.description}"
+            />
+        </a>
+    </div>    
+    `;
+}).join('');
+
+refs.divGallery.insertAdjacentHTML("afterbegin", itemsImages);
 
 console.log(galleryItems);
