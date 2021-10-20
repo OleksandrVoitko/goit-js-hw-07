5153d198e0eb1b6d05d69949a131c29e41d6ce9c
OleksandrVoitko <voytkomail@gmail.com>
2021-10-20 21:49:12 +0300

    Задание 2 - готово

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const divGalleryRef = document.querySelector(".gallery");
+
+const itemsImages = galleryItems
+  .map((item) => {
+    return `
+    <a class="gallery__item" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}"
+      title="${item.description}" />
+    </a> 
+    `;
+  })
+  .join("");
+
+divGalleryRef.insertAdjacentHTML("afterbegin", itemsImages);
+
+divGalleryRef.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+  e.preventDefault();
+
+  let gallery = new SimpleLightbox(".gallery a", { captions: true });
+}
