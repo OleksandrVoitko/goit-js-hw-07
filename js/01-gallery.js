45867737b19f99f092acf2587b6e09e6352e6f5c
OleksandrVoitko <voytkomail@gmail.com>
2021-10-20 14:23:44 +0300

    Задание 1 - готово

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,5 @@
 import { galleryItems } from "./gallery-items.js";
 
-
 const refs = {
   divGallery: document.querySelector(".gallery"),
 };
@@ -27,9 +26,27 @@ refs.divGallery.insertAdjacentHTML("afterbegin", itemsImages);
 refs.divGallery.addEventListener("click", onImageClick);
 
 function onImageClick(e) {
-    e.preventDefault();
-  return e.target.dataset.source;
+  e.preventDefault();
+
+  const imageUrl = getImageUrl(e);
+  const instance = basicLightbox.create(
+    `<img src="${imageUrl}" width="800" height="600">`,
+    {
+      onShow: (instance) => {
+        const options = { once: true };
+        function onEventHandler(e) {
+          if (e.code === "Escape") instance.close();
+        }
+        window.addEventListener("keydown", onEventHandler, options);
+      },
+    }
+  );
+
+  instance.show();
 }
 
+function getImageUrl(e) {
+  return e.target.dataset.source;
+}
 
 console.log(galleryItems);
